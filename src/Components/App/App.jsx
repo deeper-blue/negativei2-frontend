5e46f63c3883ac6a7b2fcc728b911ea16d408848
Daniel Patterson <s1643689@jackson.inf.ed.ac.uk>
2019-01-23 15:23:53 +0000

    :))

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={/public/assets/logo.svg} className="App-logo" alt="logo" />
+          <img src='/public/assets/logo.svg' className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
