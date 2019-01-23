01bd428a695a7abdbab4c565d538a14f99f1cb4a
Daniel Patterson <s1643689@jackson.inf.ed.ac.uk>
2019-01-23 16:03:11 +0000

    untested addition of restructured homepage

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from '../Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src='/assets/logo.svg' className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={Home} />
+        </Switch>
       </div>
     );
   }
