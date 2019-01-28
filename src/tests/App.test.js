d36ef1370cd192ccc146c8da3448c0fb6798ec17
Edwin Onuonga <edwinonuonga@gmail.com>
2019-01-28 22:40:03 +0000

    Move tests to src/tests directory

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App';
+import App from '../Components/App';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
@@ -9,7 +9,7 @@ it('renders without crashing', () => {
     <BrowserRouter>
         <Route component={App} />
     </BrowserRouter>
-  );  
+  );
   ReactDOM.render(app, div);
   ReactDOM.unmountComponentAtNode(div);
 });
