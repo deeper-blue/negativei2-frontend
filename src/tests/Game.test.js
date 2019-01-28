09923e0e8f4708146d6a7d3f01505f8bf91c6f76
Edwin Onuonga <edwinonuonga@gmail.com>
2019-01-28 22:44:33 +0000

    Add test file for Game component

---
+++ src/tests/Game.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from '../components/Game';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  const app = (
+    <BrowserRouter>
+        <Route component={Game} />
+    </BrowserRouter>
+  );
+  ReactDOM.render(app, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
