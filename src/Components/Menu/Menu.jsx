6220f94b999dcdaec546a5ee3e425fbadc7ebf64
Daniel <danielpatterson13@gmail.com>
2019-01-31 19:35:11 +0000

    Added menu page with two links

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Menu.scss';
 
 function Menu (props) {
 
     return (
-        <div>sadf</div>
+        <div className="menu">
+            <div>
+                <img src='/assets/logo.svg' className="App-logo" alt="logo" />
+            </div>
+            <div>
+                <div id="playGame"><Link to='/Profile'>Play GAME!</Link></div>
+                <div id="profile"><Link to='/Profile'>Profile</Link></div>
+            </div>
+        </div>
     )
 }
 
