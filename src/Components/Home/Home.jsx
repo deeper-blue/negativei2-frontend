74f21d2e251b47ccfe344f38cc6c416f45180320
Daniel <danielpatterson13@gmail.com>
2019-02-01 16:30:22 +0000

    added pages and routing for most pages

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,7 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 const fbUiConfig = {
     signInFlow: 'popup',
-    signInSuccessUrl: '/profile',
+    signInSuccessUrl: '/profile/1',
     signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         firebase.auth.GoogleAuthProvider.PROVIDER_ID
@@ -19,7 +19,7 @@ function Home (props) {
         <header className="App-header">
           <img src='/assets/logo.svg' className="App-logo" alt="logo" />
           <StyledFirebaseAuth uiConfig={fbUiConfig} firebaseAuth={firebase.auth()} />
-          <Link to='/profile'>
+          <Link to='/profile/1'>
             Play as guest
           </Link>
         </header>
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Login() {
+    return <div>asdlk</div>;
+}
+
+export default Login;
