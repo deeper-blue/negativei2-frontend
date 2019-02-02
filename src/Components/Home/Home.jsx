789e5c9184979288e7027e4cc7ab94dcac61ef39
Edwin Onuonga <edwinonuonga@gmail.com>
2019-02-02 20:39:00 +0000

    Clean up and fix links on homepage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,41 +6,14 @@ function Home (props) {
       <div className="App">
         <header className="App-header">
           <img src='/assets/logo.svg' className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="/Profile"
-          >
+          <a className="App-link" href="/profile">
             Test Profile Page
           </a>
-          <a
-            className="App-link"
-            href="/page2"
-          >
+          <a className="App-link" href="/page2">
             Test Page 2
           </a>
-          <a
-            className="App-link"
-            href="/page3"
-          >
+          <a className="App-link" href="/page3">
             Test Page 3
-          </a>
-          <a
-            className="App-link"
-            href="/page4"
-          >
-            Test Page 4
-          </a>
-          <a
-            className="App-link"
-            href="/page5"
-          >
-            Test Page 5
-          </a>
-          <a
-            className="App-link"
-            href="/page6"
-          >
-            Test Page 6
           </a>
         </header>
       </div>
