b312e440409f575f0e5e0b5f8456f86b69fd59e9
Sarah Houcheimi <Sarah.houcheimi@gmail.com>
2019-02-03 16:46:50 +0000

    Create config page and styling

---
+++ src/Components/Create/Create.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import './Create.scss'
+import { Link } from 'react-router-dom'
+
+class Create extends Component {
+
+      constructor(props) {
+        super(props);
+        // this.state = ;
+        //this.handleClick = this.handleClick.bind(this);
+      }
+
+      // handleClick(event) {
+      //   alert('Your game will be ' + this.state.value + ' long.');
+      //   event.preventDefault();
+      // }
+
+      render() {
+        return (
+          <form id="config_form">
+            <br />
+            <br />
+            <label>
+              Player 1 (White):
+              <br />
+              <select>
+                <option value="me">Me, myself and I</option>
+                <option value="robot">Deeper Blue</option>
+                <option value="human">Another human</option>
+              </select>
+            </label>
+            <br />
+            <br />
+            <label>
+              Player 2 (Black):
+              <br />
+              <select>
+                <option value="me">Me, myself and I</option>
+                <option value="robot">Deeper Blue</option>
+                <option value="human">Another human</option>
+              </select>
+            </label>
+            <br />
+            <br />
+            <br />
+            <label>
+            Game clock:
+            </label>
+            <br />
+            <label>
+              <input id = "config_input"
+                name="Hours"
+                type="number" />
+                <br />
+                 hours and
+            </label>
+            <br />
+            <label>
+              <input id = "config_input"
+                name="Minutes"
+                type="number"/>
+                <br />
+                 minutes
+            </label>
+            <br />
+            <br />
+            <br />
+            <button id="btn_play">
+                <Link to='/play' id ="btn">
+                  LET'S PLAY!!
+                </Link>
+            </button>
+          </form>
+        );
+      }
+    }
+
+export default Create;
