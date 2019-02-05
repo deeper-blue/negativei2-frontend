e72e735c87f51153a94b7fa06ebd51b93de0ef41
Daniel Patterson <danielpatterson13@gmail.com>
2019-02-05 16:15:16 +0000

    Filled table with gummy data

--- src/Components/Profile/Table/Table.jsx
+++ src/Components/Profile/Table/Table.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class Table extends Component {
 
-        
-
     render(){
+
+        const Table = styled.table`
+            width: 100%;
+        `;
+
+        const TR = styled.tr`
+
+            border: 2px solid black;
+
+        `;
+
         return (
             <div>
-                asdf
+                <Table>
+                    <TR>
+                        <th>Match ID</th>
+                        <th>Winner</th>
+                        <th>Opponent</th>
+                    </TR>
+                    <tr>
+                        <th>2352542</th>
+                        <th>White</th>
+                        <th>Dave</th>
+                    </tr>
+                    <tr>
+                        <th>2342344</th>
+                        <th>Black</th>
+                        <th>Andy</th>
+                    </tr>
+                </Table>
             </div>
         );
     }
