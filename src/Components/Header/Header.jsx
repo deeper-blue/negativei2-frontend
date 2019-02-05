4645003eff6f36aeebb17265bbeb4a9d749d648c
Edwin Onuonga <edwinonuonga@gmail.com>
2019-02-05 00:42:42 +0000

    Integrate header and nav bar with firebase auth

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import './Header.scss';
+import { auth } from '../Firebase';
 
 class Header extends Component {
     navToggled = false;
+
+    constructor(...args) {
+        super(...args);
+
+        this.state = {user: null}
+        this.initAuthListener = this.initAuthListener.bind(this);
+    }
+
+    componentDidMount() {
+        this.initAuthListener();
+    }
+
+    initAuthListener() {
+        var header = this;
+        auth.onAuthStateChanged(function(user) {
+            if (user) { // User
+                header.setState({user: user});
+            } else { // Guest
+
+            }
+        });
+    }
 
     navIcon = () => {
         if (this.navToggled === false) {
@@ -35,16 +58,19 @@ class Header extends Component {
                         HOME
                     </a>
                     <nav id="dock">
-                        <div id="dock-auth" className="dock-item" tooltip="Login/Logout" tooltip-position="bottom">
-                            <a href="/login">
+                        <div id="dock-auth" className="dock-item" tooltip={this.state.user ? 'Logout' : 'Login'} tooltip-position="bottom">
+                            <a href={this.state.user ? '/logout' : '/login'}>
                                 <img src="/assets/header/auth.png" alt="Auth" id="dock-auth-icon"></img>
                             </a>
                         </div>
+                        {this.state.user ?
                         <div id="dock-profile" className="dock-item" tooltip="Profile" tooltip-position="bottom">
                             <a href="/profile">
                                 <img src="/assets/header/profile.png" alt="Profile" id="dock-profile-icon"></img>
                             </a>
                         </div>
+                            : ''
+                        }
                         <div id="dock-burger" className="dock-item" onClick={this.navIcon}>
                             <div className="bar-1"></div>
                             <div className="bar-2"></div>
@@ -60,10 +86,12 @@ class Header extends Component {
                     <a href="/create">New game</a>
                     <a href="/join">Join game</a>
                     <hr />
-                    <p className="nav-title">User <span>abc123</span></p>
-                    <a href="/login">Login</a>
-                    <a href="/logout">Logout</a>
-                    <a href="/profile">View profile</a>
+                    <p className="nav-title">{this.state.user ? <span>{this.state.user.displayName}</span> : 'Guest'}</p>
+                    <a
+                      href={this.state.user ? '/logout' : '/login'}>
+                      {this.state.user ? 'Logout' : 'Login'}
+                    </a>
+                    {this.state.user ? <a href="/profile">View profile</a> : ''}
                 </div>
             </div>
         );
