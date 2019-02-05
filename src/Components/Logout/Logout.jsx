996746ba20c506dd415804535ba648c1bca4c366
Edwin Onuonga <edwinonuonga@gmail.com>
2019-02-05 18:55:02 +0000

    Simplify Logout component

--- src/Components/Logout/Logout.jsx
+++ src/Components/Logout/Logout.jsx
@@ -3,43 +3,14 @@ import { Redirect } from 'react-router-dom'
 import { auth } from '../Firebase';
 
 class Logout extends Component {
-    constructor(...args) {
-        super(...args);
-
-        this.state = {user: null};
-        this.initAuthListener = this.initAuthListener.bind(this);
-    }
-
-    componentDidMount() {
-        this.initAuthListener();
-    }
-
-    initAuthListener() {
-        var header = this;
-        auth.onAuthStateChanged(function(user) {
-            if (user) { // User
-                header.setState({user: user});
-            }
-        });
-    }
-
-    redirect = (to) => {
-        return <Redirect to={to} />
-    }
-
     render() {
-        if (this.state.user) { // Authenticated
-            auth.signOut().then(function() {
-                // Sign-out successful.
-                return this.redirect('/login');
-            }, function(error) {
-                // An error happened.
-                console.log(error);
-                return this.redirect('/login');
-            });
-        } else { // Unauthenticated
-            return this.redirect('/login');
-        }
+        auth.signOut().then(function() {
+            // Sign-out successful.
+        }, function(error) {
+            // An error happened.
+            console.log(error);
+        });
+        return <Redirect to="/login" />;
     }
 }
 
