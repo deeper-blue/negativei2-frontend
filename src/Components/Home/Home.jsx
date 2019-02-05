63070cd2af811b30552c51f68e3968a321dc6f41
Cameron MacLeod <cmacleod170@googlemail.com>
2019-02-05 15:25:04 +0000

    Create/play links on home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,9 +3,10 @@ import './Home.scss';
 
 function Home (props) {
     return (
-      <div>
-        CHESS
-      </div>
+        <div className='home-links'>
+            <Link to='/create' className='home-link'>Create game</Link>
+            <Link to='/play' className='home-link'>Play game</Link>
+        </div>
     );
 }
 
