a61f53539675b75cec1d8b3f2d55c6c720491cb4
Edwin Onuonga <edwinonuonga@gmail.com>
2019-02-05 18:41:44 +0000

    Add component files for logging out

---
+++ src/Components/Logout/Logout.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom'
+import { auth } from '../Firebase';
+
+class Logout extends Component {
+    constructor(...args) {
+        super(...args);
+
+        this.state = {user: null};
+        this.initAuthListener = this.initAuthListener.bind(this);
+    }
+
+    componentDidMount() {
+        this.initAuthListener();
+    }
+
+    initAuthListener() {
+        var header = this;
+        auth.onAuthStateChanged(function(user) {
+            if (user) { // User
+                header.setState({user: user});
+            }
+        });
+    }
+
+    redirect = (to) => {
+        return <Redirect to={to} />
+    }
+
+    render() {
+        if (this.state.user) { // Authenticated
+            auth.signOut().then(function() {
+                // Sign-out successful.
+                return this.redirect('/login');
+            }, function(error) {
+                // An error happened.
+                console.log(error);
+                return this.redirect('/login');
+            });
+        } else { // Unauthenticated
+            return this.redirect('/login');
+        }
+    }
+}
+
+export default Logout;
