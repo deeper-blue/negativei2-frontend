67ff8c96ccd278f3a87ddf2a1c104d81caab2e0d
Daniel Patterson <danielpatterson13@gmail.com>
2019-02-25 15:02:26 +0000

    initial httprequest success

--- src/Components/Join/Join.jsx
+++ src/Components/Join/Join.jsx
@@ -1,25 +1,65 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Join.scss';
+import axios from 'axios';
 
-function Join (props) {
-    return (
-        <div className='matches'>
-            <h1>Open matches</h1>
-            <table className="match-list">
-                <tr>
-                    <td><b>Creator</b></td>
-                    <td><b>Open slots</b></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>@notexactlyawe</td>
-                    <td>1</td>
-                    <td><Link to='/play'>Play!</Link></td>
-                </tr>
-            </table>
-        </div>
-    );
+const sum = 'http://negativei2-server.herokuapp.com/getgame/sadjlsda'
+
+class Join extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            doodoo: ''
+        }
+    }
+
+    componentDidMount() {
+        this.somehttprequestshit(sum);
+
+
+    }
+
+    somehttprequestshit(url){
+        axios.get(url)
+            .then(function(response) {
+                console.log(response);
+                this.fuck(response);
+            }.bind(this))
+            .catch( function (error) {
+                console.log(error);
+            })
+            .then(function (){
+                console.log('poop');
+            });
+    }
+
+    fuck(shit) {
+        this.setState( state => ({
+            doodoo: shit
+        }))
+    }   
+
+    render() {
+        return (
+            <div className='matches'>
+                <h1>Open matches</h1>
+                <table className="match-list">
+                    <tr>
+                        <td><b>Creator</b></td>
+                        <td><b>Open slots</b></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>{this.state.doodoo.statusText}</td>
+                        <td>1</td>
+                        <td><Link to='/play'>Play!</Link></td>
+                    </tr>
+                </table>
+            </div>
+        );
+    }
 }
 
 export default Join;
