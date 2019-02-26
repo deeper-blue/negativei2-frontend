c62c9ec546a6af29967b3a71777ea47a48f2f78f
Daniel Patterson <danielpatterson13@gmail.com>
2019-02-26 18:34:51 +0000

    Delayed loading for firebase authentication

--- src/Components/Play/Play.jsx
+++ src/Components/Play/Play.jsx
@@ -3,45 +3,79 @@ import Chessboard from 'chessboardjsx';
 import './Play.scss';
 import Sidebar from './Sidebar';
 import HumanVsHuman from './HumanVsHuman';
+import Spinner from '../Spinner';
+import { auth } from '../Firebase';
 
 class Play extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            user: null,
+            loaded: false
+        }
+    }
+
+    componentDidMount() {
+        this.initAuthListener();
+    }
+
+    initAuthListener(){
+        auth.onAuthStateChanged(function(user) {
+            if (user) {
+                this.setState({user: user.uid});
+            } else {
+                this.setState({user: 'none'});
+            }
+        }.bind(this))
+    }
+
     render() {
         return (
-            <div className="game-area">
-                <HumanVsHuman gameid={this.props.location.pathname.split('/')[2]} userid=''>
-                {({
-                    position,
-                    onDrop,
-                    onMouseOverSquare,
-                    onMouseOutSquare,
-                    squareStyles,
-                    dropSquareStyle,
-                    onDragOverSquare,
-                    onSquareClick,
-                    onSquareRightClick
-                }) => (
-                    <Chessboard
-                    id="humanVsHuman"
-                    calcWidth={({ screenWidth, screenHeight }) => (screenWidth || screenHeight) < 550 ? 300 : 500}
-                    position={position}
-                    onDrop={onDrop}
-                    onMouseOverSquare={onMouseOverSquare}
-                    onMouseOutSquare={onMouseOutSquare}
-                    boardStyle={{
-                        borderRadius: '5px',
-                        boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
-                    }}
-                    squareStyles={squareStyles}
-                    dropSquareStyle={dropSquareStyle}
-                    onDragOverSquare={onDragOverSquare}
-                    onSquareClick={onSquareClick}
-                    onSquareRightClick={onSquareRightClick}
-                    darkSquareStyle={{backgroundColor: 'grey'}}
-                    lightSquareStyle={{backgroundColor: 'lightgrey'}}
-                    />
-                )}
-                </HumanVsHuman>
-                <Sidebar />
+            <div>
+                {
+                    this.state.user ? 
+                    <div className='game-area'>
+                        <HumanVsHuman gameid={this.props.location.pathname.split('/')[2]} userid=''>
+                        {({
+                            position,
+                            onDrop,
+                            onMouseOverSquare,
+                            onMouseOutSquare,
+                            squareStyles,
+                            dropSquareStyle,
+                            onDragOverSquare,
+                            onSquareClick,
+                            onSquareRightClick
+                        }) => (
+                            <Chessboard
+                            id="humanVsHuman"
+                            calcWidth={({ screenWidth, screenHeight }) => (screenWidth || screenHeight) < 550 ? 300 : 500}
+                            position={position}
+                            onDrop={onDrop}
+                            onMouseOverSquare={onMouseOverSquare}
+                            onMouseOutSquare={onMouseOutSquare}
+                            boardStyle={{
+                                borderRadius: '5px',
+                                boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
+                            }}
+                            squareStyles={squareStyles}
+                            dropSquareStyle={dropSquareStyle}
+                            onDragOverSquare={onDragOverSquare}
+                            onSquareClick={onSquareClick}
+                            onSquareRightClick={onSquareRightClick}
+                            darkSquareStyle={{backgroundColor: 'grey'}}
+                            lightSquareStyle={{backgroundColor: 'lightgrey'}}
+                            />
+                        )}
+                        </HumanVsHuman>
+                        <Sidebar />
+                    </div> : 
+                    <div>
+                        <Spinner />
+                    </div>
+                }
             </div>
         );
     }
