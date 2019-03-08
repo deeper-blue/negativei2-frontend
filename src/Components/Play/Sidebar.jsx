5dc66665eb7d5f82f859232a2fe612fa0fcfcf6f
Edwin Onuonga <edwinonuonga@gmail.com>
2019-03-08 01:10:04 +0000

    Add help tooltips for speech and text input

--- src/Components/Play/Sidebar.jsx
+++ src/Components/Play/Sidebar.jsx
@@ -22,17 +22,25 @@ function Sidebar (props) {
             </div>
             <div id="manual-move-input">
                 <form id="move-input-form">
-                    <input id="move-input-text" type="text" placeholder="Enter move..."></input>
+                    <input
+                    id="move-input-text"
+                    type="text"
+                    placeholder="Enter move..."
+                    ></input>
                     <input id="move-input-submit" type="submit" name="input-move" value="Move!" className="button small small-font"></input>
                 </form>
-                <div id="toggle-speech-input-wrapper">
-                    <div id="toggle-speech-input">
-                        <img src="/assets/game/microphone.png" alt="M"></img>
+                <div id="manual-input-help-wrapper">
+                    <div
+                    id="manual-input-help"
+                    tooltip-large="Moves can be input by specifying the source and target squares, e.g. 'e2e4' (even 'e2 e4'), or 'f6e4'."
+                    tooltip-position="left"
+                    >
+                        <img src="/assets/game/question-mark.png" alt="?"></img>
                     </div>
                 </div>
-                <div id="manual-input-help-wrapper">
-                    <div id="manual-input-help">
-                        <img src="/assets/game/question-mark.png" alt="?"></img>
+                <div id="toggle-speech-input-wrapper">
+                    <div id="toggle-speech-input" tooltip-large="Toggle speech input" tooltip-position="left">
+                        <img src="/assets/game/microphone.png" alt="M"></img>
                     </div>
                 </div>
             </div>
