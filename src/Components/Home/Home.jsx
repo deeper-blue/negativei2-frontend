a47ce620b7a2b5da9fabc6a14cb8aed575a0f938
daniel <danielpatterson13@gmail.com>
2019-03-10 19:34:20 +0000

    home redirects to login for non signed in users

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,61 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { auth } from '../Firebase';
+import Spinner from '../Spinner';
 import './Home.scss';
 
-function Home (props) {
-    return (
-        <div className='home-links'>
-            <Link to='/create' className="button large large-font home-link">
-                Create game
-            </Link>
-            <Link to='/join' className="button large large-font home-link">
-                Join game
-            </Link>
-        </div>
-    );
+class Home extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            user: null
+        }
+    }
+
+    componentDidMount(){
+        this.initAuthListener();
+    }
+
+    initAuthListener(){
+        auth.onAuthStateChanged(function (user) {
+            if (user) {
+                this.setState({user: user.uid});
+            } else {
+                this.setState({user: 'none'});
+            }
+        }.bind(this));
+    }
+
+    render(){
+        return(
+            <div>
+                {
+                    this.state.user 
+                ?
+                    <div>
+                        {
+                            this.state.user == 'none' 
+                        ?
+                            <Redirect to='/login'/>
+                        :
+                            <div className='home-links'>
+                                <Link to='/create' className="button large large-font home-link">
+                                    Create game
+                                </Link>
+                                <Link to='/join' className="button large large-font home-link">
+                                    Join game
+                                </Link>
+                            </div>
+                        }
+                    </div>
+                : 
+                    <Spinner />
+                }
+            </div>
+        );
+    }
 }
 
 export default Home;
