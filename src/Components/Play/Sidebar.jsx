4de115ca670c1af4c3970aa307ef35c56a438fd3
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-03-11 18:02:06 +0000

    Implement the layout of the draw and forfeit buttons on the sidebar.

--- src/Components/Play/Sidebar.jsx
+++ src/Components/Play/Sidebar.jsx
@@ -55,6 +55,47 @@ function Sidebar (props) {
                     </tbody>
                 </table>
             </div>
+            <div id="draw-forfeit-buttons">
+                <button id="draw-button" className="button small small-font">Draw</button>
+                <div className="divider"/>
+                <button id="forfeit-button" className="button small small-font">Forfeit</button>
+            </div>
+            <div id="draw-offer-confirmation" className="confirmation-box">
+                <div id="draw-offer" className="confirmation-content">
+                    <p>Send draw offer?</p>
+                    <button id="send-draw" className="button small small-font">Send</button>
+                    <div className="divider"/>
+                    <button id="cancel-draw" className="button small small-font">Cancel</button>
+                </div>
+            </div>
+            <div id="forfeit-offer-confirmation" className="confirmation-box">
+                <div id="forfeit-confirmation" className="confirmation-content">
+                    <p>Forfeit game?</p>
+                    <button id="send-forfeit" className="button small small-font">Forfeit</button>
+                    <div className="divider"/>
+                    <button id="cancel-forfeit" className="button small small-font">Cancel</button>
+                </div>
+            </div>
+            <div id="draw-received-confirmation" className="confirmation-box">
+                <div id="draw-received" className="confirmation-content">
+                    <p>Your opponent offers you a draw.</p>
+                    <button id="accept-draw" className="button small small-font">Accept</button>
+                    <div className="divider"/>
+                    <button id="decline-offer" className="button small small-font">Decline</button>
+                </div>
+            </div>
+            <div id="draw-rejection-confirmation" className="confirmation-box">
+                <div id="rejection-confirmation" className="confirmation-content">
+                    <p>Your opponent rejects your draw offer.</p>
+                    <button id="close-rejection" className="button small small-font">Close</button>
+                </div>
+            </div>
+            <div id="opponent-forfeit-confirmation" className="confirmation-box">
+                <div id="opponent-forfeit" className="confirmation-content">
+                    <p>Your opponent forfeits the game.</p>
+                    <button id="close-forfeit" className="button small small-font">Close</button>
+                </div>
+            </div>
         </div>
     );
 }
