47c1407d6081947f4f4d2528d13235293f32aed2
Daniel Patterson <danielpatterson13@gmail.com>
2019-03-11 13:01:58 +0000

    created dummy creation component

---
+++ src/Components/Profile/Creation/Creation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Creation extends React.Component {
+
+
+
+    render() {
+        return(
+            <div>
+                sadf
+            </div>
+        )
+    }
+}
+
+export default Creation;
