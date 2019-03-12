ddfd5becf091ecc9c6ab11e0f1e13bf35d97441e
Edwin Onuonga <edwinonuonga@gmail.com>
2019-03-12 17:54:23 +0000

    Change styling on /home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,14 +6,17 @@ function Home (props) {
     document.title = 'Deeper Blue: Home';
 
     return (
-        <div className='home-links'>
+        <div id="wrapper">
             <img src="/assets/deeper-blue/robot-banner/banner-alpha.svg" alt="banner" id="banner"></img>
-            <Link to='/create' className="button large large-font home-link">
-                Create game
-            </Link>
-            <Link to='/join' className="button large large-font home-link">
-                Join game
-            </Link>
+
+            <div className='home-links'>
+                <Link to='/create' className="button large large-font home-link">
+                    Create game
+                </Link>
+                <Link to='/join' className="button large large-font home-link">
+                    Join game
+                </Link>
+            </div>
         </div>
     );
 }
