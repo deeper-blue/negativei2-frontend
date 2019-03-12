4a84ae987771774b8d8645afa68bf1e4891394f4
Edwin Onuonga <edwinonuonga@gmail.com>
2019-03-12 02:11:31 +0000

    Add support for speech input

--- src/Components/Play/Sidebar.jsx
+++ src/Components/Play/Sidebar.jsx
@@ -2,13 +2,129 @@ import React, { Component } from 'react';
 import './Sidebar.scss'
 
 class Sidebar extends Component {
-    currentTab = 'game';
+    SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+    SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList
+    SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent
 
+    state = {
+        currentTab: 'game',
+        speechSupported: true,
+        speechToggled: false
+    }
+
+    componentDidMount() {
+        try {
+            // Try to set up speech recognition
+            this.recognition = new this.SpeechRecognition();
+            this.recognition.lang = 'en-US';
+            this.recognition.interimResults = false;
+            this.recognition.maxAlternatives = 1;
+            this.recognition.continuous = true;
+        } catch(error) {
+            if (error instanceof TypeError) {
+                // Speech input not supported
+                this.setState({speechSupported: false}, () => {
+                    let toggle = document.getElementById('toggle-speech-input');
+                    toggle.classList.toggle('disabled');
+                    toggle.setAttribute("tooltip-large", 'Speech input is not supported on your browser.');
+                });
+            } else {
+                throw error;
+            }
+        }
+    }
+
+    /** Toggles speech input. */
+    toggleSpeech = () => {
+        var toggle = document.getElementById('toggle-speech-input');
+        var moveSubmit = document.getElementById('move-input-form');
+
+        if (this.state.speechSupported) {
+            toggle.classList.toggle('change');
+            if (this.state.speechToggled) {
+                // Disabled
+                toggle.setAttribute("tooltip-large", "Enable speech input");
+                moveSubmit.removeAttribute("tooltip-large")
+                this.setState({speechToggled: false}, this.handleSpeech);
+            } else {
+                // Enabled
+                toggle.setAttribute("tooltip-large", "Disable speech input");
+                moveSubmit.setAttribute("tooltip-large", "You can submit a move with speech by saying 'move' after specifying the move.");
+                this.setState({speechToggled: true}, this.handleSpeech);
+            }
+            document.getElementById('move-input-text').value = '';
+        }
+    }
+
+    /** Cleans a transcript by lowercasing and performing a substitution
+     * according to a map of common homophones and similar incorrect words.
+     * @param {string} transcript - The transcript to clean.
+     */
+    cleanTranscript = (transcript) => {
+        transcript = transcript.toLowerCase();
+
+        // Substitution map for homophones or similar incorrect words
+        var substitutionMap = {
+            eh: 'a',
+            bee: 'b', be: 'b',
+            see: 'c', sea: 'c',
+            one: '1', won: '1',
+            two: '2', to: '2', too: '2',
+            three: '3', tree: '3', free: '3',
+            four: '4', fore: '4', for: '4',
+            five: '5', hive: '5',
+            six: '6', sex: '6', sax: '6',
+            seven: '7',
+            eight: '8', ate: '8', hate: '8'
+        }
+
+        // Replace any words present in the substitution map
+        Object.keys(substitutionMap).forEach(function (incorrect) {
+            transcript = transcript.replace(new RegExp(incorrect, 'gi'), substitutionMap[incorrect]);
+        });
+    }
+
+    /** Handles speech recognition and transcripts. */
+    handleSpeech = () => {
+        if (this.state.speechToggled) {
+            this.recognition.start();
+
+            var input = document.getElementById('move-input-text');
+
+            let finalTranscript = ''
+            this.recognition.onresult = event => {
+                for (let i = event.resultIndex; i < event.results.length; i++) {
+                    const transcript = this.cleanTranscript(event.results[i][0].transcript);
+                    if (event.results[i].isFinal) finalTranscript += transcript + ' '
+                }
+
+                input.value = finalTranscript
+
+                if (finalTranscript.includes('clear')) {
+                    // Clear command
+                    finalTranscript = '';
+                    input.value = finalTranscript;
+                }
+                if (finalTranscript.includes('move')) {
+                    // Move command
+                    finalTranscript = finalTranscript.replace(/move/gi, '');
+                    input.value = finalTranscript;
+
+                    document.getElementById('move-input-submit').click();
+
+                    finalTranscript = '';
+                    input.value = finalTranscript;
+                }
+            }
+        } else {
+            this.recognition.stop();
+        }
+    }
 
     openTab = t => {
-        if (this.currentTab === t) return;
+        if (this.state.currentTab === t) return;
 
-        this.currentTab = t;
+        this.setState({currentTab: t});
 
         var tab = `${t}-tab`;
 
@@ -23,14 +139,6 @@ class Sidebar extends Component {
         }
 
         document.getElementById(tab).style.display = "block";
-    }
-
-    handleSubmit = event => {
-        event.preventDefault();
-        const data = new FormData(event.target);
-        var move = data.get('move');
-        // TODO: Actually make the requested move.
-        console.log(move);
     }
 
     render() {
@@ -71,21 +179,27 @@ class Sidebar extends Component {
                                 </div>
                             </div>
                             <div id="manual-move-input">
-                                <form id="move-input-form" onSubmit={this.handleSubmit}>
+                                <form id="move-input-form">
                                     <input id="move-input-text" type="text" placeholder="Enter move..."></input>
-                                    <input id="move-input-submit" type="submit" name="input-move" value="Move!" className="button small small-font"></input>
+                                    <input
+                                    id="move-input-submit"
+                                    type="submit"
+                                    name="input-move"
+                                    value="Move!"
+                                    className="button small small-font"
+                                    tooltip-position="top"></input>
                                 </form>
                                 <div id="manual-input-help-wrapper">
                                     <div
                                     id="manual-input-help"
-                                    tooltip-large="Moves can be input by specifying the source and target squares, e.g. 'e2e4' (even 'e2 e4'), or 'f6e4'."
-                                    tooltip-position="left"
+                                    tooltip-very-large="Moves can be input by specifying the source and target squares, e.g. 'e2e4' (even 'e2 e4'), or 'f6e4'."
+                                    tooltip-position="top"
                                     >
                                         <img src="/assets/game/question-mark.png" alt="?"></img>
                                     </div>
                                 </div>
                                 <div id="toggle-speech-input-wrapper">
-                                    <div id="toggle-speech-input" tooltip-large="Toggle speech input" tooltip-position="left">
+                                    <div id="toggle-speech-input" onClick={this.toggleSpeech} tooltip-large="Enable speech input" tooltip-position="left">
                                         <img src="/assets/game/microphone.png" alt="M"></img>
                                     </div>
                                 </div>
