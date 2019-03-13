e93d596a6b134a7080b7813987b4c8b27810b05c
Daniel Patterson <danielpatterson13@gmail.com>
2019-03-13 13:45:54 +0000

    Added bind

--- src/Components/Profile/Creation/Creation.jsx
+++ src/Components/Profile/Creation/Creation.jsx
@@ -84,7 +84,7 @@ class Creation extends React.Component {
             .then(function(){
                 console.log('Document updates');
                 this.props.history.push('/');
-            })
+            }.bind(this))
             .catch(function(error) {
                 console.error('error updating document', error);
             })
