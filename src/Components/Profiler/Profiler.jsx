d5e9bf5115a9f9c0fb0f3654293074b4ffa39c6e
Daniel Patterson <danielpatterson13@gmail.com>
2019-03-22 02:45:37 +0000

    profiler now redirects to login page for anon users

--- src/Components/Profiler/Profiler.jsx
+++ src/Components/Profiler/Profiler.jsx
@@ -8,7 +8,8 @@ class Profiler extends React.Component {
         super(props);
 
         this.state = {
-            user: null
+            user: null,
+            anon: null
         }
     }
 
@@ -19,9 +20,14 @@ class Profiler extends React.Component {
     initAuthListener(){
         auth.onAuthStateChanged(function(user) {
             if (user) {
-                this.setState({user: user.uid});
+                this.setState({
+                    user: user.uid,
+                    anon: user.isAnonymous
+                });
             } else {
-                this.setState({user: 'none'});
+                this.setState({
+                    user: 'none'
+                });
             }
         }.bind(this))
     }
@@ -29,9 +35,13 @@ class Profiler extends React.Component {
     render(){
         return(
             <div>
-                {this.state.user ? 
-                <Redirect to={this.state.user === 'none' ? '/login' : '/profile/' + this.state.user}/> : 
-                <div>redirecting...</div>}
+                {
+                    this.state.user 
+                ? 
+                    <Redirect to={this.state.user === 'none' ? '/login' : '/profile/' + this.state.user}/>
+                : 
+                    <div>redirecting...</div>
+                }
             </div>
         )
     }
