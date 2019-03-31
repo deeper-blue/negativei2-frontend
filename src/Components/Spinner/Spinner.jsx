e1bbdcef53274387fd347959739257f5bdcf4f59
Edwin Onuonga <edwinonuonga@gmail.com>
2019-03-31 19:43:51 +0100

    Modify Spinner to display arbitrary text

--- src/Components/Spinner/Spinner.jsx
+++ src/Components/Spinner/Spinner.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import './Spinner.scss'
 
 class Spinner extends React.Component {
+    constructor(props){
+        super(props);
+        this.text = this.props.children || 'Loading...'
+    }
+
     render() {
         return (
             this.props.fullPage
             ?
             <div id='spinner' class='full-page'>
                 <img src='/assets/spinner.jpg' alt='' />
-                <h1>Loading...</h1>
+                <h1>{this.text}</h1>
             </div>
             :
             <div id='spinner'>
                 <img src='/assets/spinner.jpg' alt='' />
-                <h1>Loading...</h1>
+                <h1>{this.text}</h1>
             </div>
         );
     }
