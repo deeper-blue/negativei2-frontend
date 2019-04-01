a8048c9f090d87c22d6787ad73b21f7d156d8821
Daniel Patterson <danielpatterson13@gmail.com>
2019-04-01 18:46:52 +0100

    anon improvements (#113)
    * changed anon users to appear to not yet be signed in
    
    * profiler now redirects to login page for anon users
    
    * Guests in games now display correctly on join page
    
    * Changed 'Creator ID' to 'Creator' in join page table
    
    * Added guest names to creator column

--- src/Components/Join/Join.jsx
+++ src/Components/Join/Join.jsx
@@ -20,6 +20,7 @@ class Join extends React.Component {
 
         this.semaphore = 0;
         this.getUsername = this.getUsername.bind(this);
+        this.getNameFromId = this.getNameFromId.bind(this);
     }
 
     componentDidMount() {
@@ -93,6 +94,7 @@ class Join extends React.Component {
 
         docRef.get().then(function(response) {
             if(response.exists){
+                console.log(response.data.name);
                 this.user_dictionary[user_id] = response.data().name;
             }
             this.semaphore--;
@@ -113,6 +115,14 @@ class Join extends React.Component {
         this.httpPostRequest('/joingame', formData);
     }
 
+    getNameFromId(user_id) {
+        if(!this.state.user_dictionary[user_id]) {
+            return('Guest');
+        } else {
+            return(this.state.user_dictionary[user_id]);
+        }
+    }
+
     render() {
         return (
             <div>
@@ -124,7 +134,7 @@ class Join extends React.Component {
                         <thead>
                             <tr>
                                 <th>Game ID</th>
-                                <th>Creator ID</th>
+                                <th>Creator</th>
                                 <th>Open slots</th>
                                 <th>White</th>
                                 <th>Black</th>
@@ -137,10 +147,10 @@ class Join extends React.Component {
                                     row.free_slots = 0 ? null :
                                     <tr>
                                         <td>{row.id}</td>
-                                        <td>{this.state.user_dictionary[row.creator]}</td>
+                                        <td>{this.getNameFromId(row.creator)}</td>
                                         <td>{row.free_slots}</td>
-                                        <td>{row.players.w ? this.state.user_dictionary[row.players.w] : <button onClick={(game_id, side, e) => this.joinGame(row.id, 'w')}>PLAY</button>}</td>
-                                        <td>{row.players.b ? this.state.user_dictionary[row.players.b] : <button onClick={(game_id, side, e) => this.joinGame(row.id, 'b')}>PLAY</button>}</td>
+                                        <td>{row.players.w ? this.getNameFromId(row.players.w) : <button onClick={(game_id, side, e) => this.joinGame(row.id, 'w')}>PLAY</button>}</td>
+                                        <td>{row.players.b ? this.getNameFromId(row.players.b) : <button onClick={(game_id, side, e) => this.joinGame(row.id, 'b')}>PLAY</button>}</td>
                                         <td>{row.time_controls}</td>
                                     </tr>
                                 ))
