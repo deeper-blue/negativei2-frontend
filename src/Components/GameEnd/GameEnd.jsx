19ce53177315941f2d68e96e108a237ca3bb2b1b
Edwin Onuonga <edwinonuonga@gmail.com>
2019-04-02 19:54:27 +0100

    Fix game result

--- src/Components/GameEnd/GameEnd.jsx
+++ src/Components/GameEnd/GameEnd.jsx
@@ -92,13 +92,13 @@ class GameEnd extends React.Component {
   }
 
   getResult = () => {
-    if (this.state.game === '1-0') {
+    if (this.state.game.result === '1-0') {
       this.setState({black: 'Win'});
       this.setState({white: 'Lose'});
-    } else if (this.state.game === '0-1') {
+    } else if (this.state.game.result === '0-1') {
       this.setState({black: 'Lose'});
       this.setState({white: 'Win'});
-    } else if (this.state.game === '1/2-1/2') {
+    } else if (this.state.game.result === '1/2-1/2') {
       this.setState({black: 'Draw'});
       this.setState({white: 'Draw'});
     } else {
