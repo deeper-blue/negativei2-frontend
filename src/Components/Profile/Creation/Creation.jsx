6e88750a1fed3797c8fe3a68ec7964cb73cf75be
Daniel Patterson <danielpatterson13@gmail.com>
2019-04-02 19:01:18 +0100

    Added validation and styling

--- src/Components/Profile/Creation/Creation.jsx
+++ src/Components/Profile/Creation/Creation.jsx
@@ -12,7 +12,8 @@ class Creation extends React.Component {
         this.state = {
             name: '',
             picture: '',
-            user: null
+            user: null,
+            errors: ''
         }
 
         this.handleNameChange = this.handleNameChange.bind(this);
@@ -27,14 +28,14 @@ class Creation extends React.Component {
     initAuthListener(){
         auth.onAuthStateChanged(function (user) {
             if (user) {
-                this.setState({user: user.uid});
+                this.setState({user: user});
                 
                 const db = firebase.firestore();
-                var docRef = db.collection('users').doc(this.state.user);
+                var docRef = db.collection('users').doc(this.state.user.uid);
 
                 docRef.get().then(function(doc) {
                     if(doc.exists) {
-                        console.logt(doc.data);
+                        console.log(doc.data, "Redirect");
                         this.props.history.push('/');
                     } else {
                         this.createProfile(user.uid.substring(0,10), 'https://i.imgur.com/TOJtdzW.png', false);
@@ -60,16 +61,22 @@ class Creation extends React.Component {
         var name = this.state.name;
         var picture = this.state.picture;
 
-        if(!name || !picture){
+        if(!this.validate(name, picture)){
+            this.setState({
+                errors: 'Ensure your username is 0-32 characters long and picture is empty or a valid image source.'
+            })
             return;
         }
+        if(picture === ''){
+            this.updateProfile(name, 'https://i.imgur.com/TOJtdzW.png')
+        }
 
-        this.updateProfile(name, picture, true);
+        this.updateProfile(name, picture);
     }
 
     createProfile(name, picture){
         const db = firebase.firestore();
-        var profileRef = db.collection('users').doc(this.state.user);
+        var profileRef = db.collection('users').doc(this.state.user.uid);
 
         var profileObj = {}
         if(this.state.user.displayName){
@@ -78,7 +85,6 @@ class Creation extends React.Component {
             profileObj['name'] = name;
         }
         profileObj['pic'] = picture;
-        console.log(profileObj);
         profileRef.set(profileObj)
             .then(function(){
                 console.log('Document updated');
@@ -90,12 +96,10 @@ class Creation extends React.Component {
 
     updateProfile(name, picture){
         const db = firebase.firestore();
-        var profileRef = db.collection('users').doc(this.state.user);
-
+        var profileRef = db.collection('users').doc(this.state.user.uid);
         var profileObj = {}
         profileObj['name'] = name;
-        profileObj['pic'] = picture;
-
+        profileObj['picture'] = picture;
         profileRef.update(profileObj)
             .then(function(){
                 console.log('Document updates');
@@ -106,36 +110,66 @@ class Creation extends React.Component {
             })
     }
 
+    validate(name, picture) {
+        var errors = []
+        errors[0] = name.length < 32;
+        errors[1] = this.validURL(picture) || picture === '';
+
+        return errors.reduce((total, bool) => total && bool);
+    }
+
+    // regular expression test for url
+    validURL(str) {
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        return !!pattern.test(str);
+    }
+    
     render() {
         return(
-            <divdiv>
+            <div>
                 {
                     this.state.user
                 ?
                     <div id="creation">
-                        <div className='display_name'>
-                            <label>
-                                Name: <input type='text' value={this.state.name} onChange={this.handleNameChange} />
-                            </label>
-                        </div>
-                        <div className='display_picture'>
-                            <label>
-                                Profile Picture: <input type='text' value={this.state.picture} onChange={this.handlePPChange} />
-                            </label>
+                        <div>
+                            Please enter your details below to create your profile. <br/>
+                            You can skip this step and change these details later from the profile page.
                         </div>
                         <div>
-                            <button onClick={this.handleProfileSubmit} className='button'>
-                                submit
-                            </button>
-                            <Link to='/' className='button'>
-                                skip this step
-                            </Link>
+                            <div id='errors'>
+                                {this.state.errors}
+                            </div>
+                            <div className='inputs'>
+                                <div className='display_name'>
+                                    <label>
+                                        Name: <input type='text' value={this.state.name} onChange={this.handleNameChange} />
+                                    </label>
+                                </div>
+                                <div className='display_picture'>
+                                    <label>
+                                        Profile Picture: <input type='text' value={this.state.picture} onChange={this.handlePPChange} />
+                                    </label>
+                                </div>
+                            </div>
+                            <div>
+                                <button onClick={this.handleProfileSubmit} className='button submission'>
+                                    submit
+                                </button>
+                                <Link to='/'>
+                                    skip this step
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 :
                     <Spinner fullPage={true}/>
                 }
-            </divdiv>
+            </div>
         )
     }
 }
